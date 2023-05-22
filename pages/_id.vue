9ddf9220c128f7d4f407d43742d046ad6b66e3bb
taufikamf <taufikabdul354@gmail.com>
2023-05-23 01:37:18 +0700

    finally the fast one

--- pages/_id.vue
+++ pages/_id.vue
@@ -1,9 +1,17 @@
+<template>
+  <div></div>
+</template>
 <script>
 export default {
-  asyncData({ redirect, params }) {
+  mounted() {
     const url = process.env.WP_URL;
-    const postId = params.id;
-    return redirect(302, `${url}${postId}`); // Replace with your WordPress URL
+    const postId = this.$route.params.id;
+    window.location.href = `${url}${postId}`;
   },
+  // asyncData({ redirect, params }) {
+  //   const url = process.env.WP_URL;
+  //   const postId = params.id;
+  //   return redirect(302, `${url}${postId}`); // Replace with your WordPress URL
+  // },
 };
 </script>
